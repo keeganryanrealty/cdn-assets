3060ff39890f3bf3b958ddd9edd01325c1d85f6c
keeganryanrealty <keegan.ryan@exprealty.com>
2025-06-25 20:07:44 +0000

    Update header styles (e.g., background color)

---
+++ js/header-dropdown.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navList = document.querySelector(".nav-primary");
+
+  if (hamburger && navList) {
+    hamburger.addEventListener("click", function () {
+      navList.classList.toggle("active");
+      hamburger.blur();
+    });
+  }
+});
