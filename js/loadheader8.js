025977b29a8c9f63996c643d2507f17e0db5d1c4
keeganryanrealty <keegan.ryan@exprealty.com>
2025-06-28 12:11:52 -0500

    Update and rename loadheader7.js to loadheader8.js

--- js/loadheader8.js
+++ js/loadheader8.js
@@ -15,11 +15,3 @@ meta.name = "viewport";
 meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
 document.head.appendChild(meta);
 
-
-
-// ABOUT ME SECTION
-  fetch("https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/about-me-2.html")
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById("about-me-placeholder").innerHTML = html;
-    });
