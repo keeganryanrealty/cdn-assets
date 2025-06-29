ce17afa9ed0c324120abe4f3e585f8d842c7f5c4
keeganryanrealty <keegan.ryan@exprealty.com>
2025-06-29 08:55:36 -0500

    Create about-me.js

---
+++ js/about-me.js
@@ -0,0 +1,5 @@
+fetch("https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/about-me.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("about-me-placeholder").innerHTML = html;
+    });
