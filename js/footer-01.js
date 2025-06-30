ccbe03e250f9e72047eb44a0903ef95cefb78024
keeganryanrealty <keegan.ryan@exprealty.com>
2025-06-30 12:32:24 -0500

    Update and rename footer.js to footer-01.js

--- js/footer-01.js
+++ js/footer-01.js
@@ -2,7 +2,7 @@
   const footerAlreadyLoaded = document.querySelector('#custom-footer');
   if (footerAlreadyLoaded) return;
 
-  const res = await fetch('https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/footer.html');
+  const res = await fetch('https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/footer-01.html');
   const html = await res.text();
   const footer = document.createElement('div');
   footer.innerHTML = html;
