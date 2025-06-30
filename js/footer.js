31cbd1c813e23cdc30972a5b642725e0f12f4ec9
keeganryanrealty <keegan.ryan@exprealty.com>
2025-06-30 12:19:56 -0500

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+(async function () {
+  const footerAlreadyLoaded = document.querySelector('#custom-footer');
+  if (footerAlreadyLoaded) return;
+
+  const res = await fetch('https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/footer.html');
+  const html = await res.text();
+  const footer = document.createElement('div');
+  footer.innerHTML = html;
+
+  document.body.appendChild(footer);
+})();
