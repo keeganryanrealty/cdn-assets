a9db6af10466c76640365f11b022c39a988e2077
keeganryanrealty <keegan.ryan@exprealty.com>
2025-06-30 10:35:10 -0500

    Update and rename loadheader08.js to loadheader-09.js

--- js/loadheader-09.js
+++ js/loadheader-09.js
@@ -15,3 +15,17 @@ meta.name = "viewport";
 meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
 document.head.appendChild(meta);
 
+
+
+// Load Hero & About Only On Correct Pages
+  document.addEventListener("DOMContentLoaded", function () {
+    const path = window.location.pathname;
+
+    const hero = document.getElementById("custom-hero-placeholder");
+    const about = document.getElementById("about-me-placeholder");
+
+    const isHome = (path === "/" || path === "");
+
+    if (hero) hero.style.display = isHome ? "block" : "none";
+    if (about) about.style.display = isHome ? "block" : "none";
+  });
