a95d9a1ecc81665da624cd8607e9cfc423bfe854
keeganryanrealty <keegan.ryan@exprealty.com>
2025-07-09 14:18:43 -0500

    Update and rename index-lakeville-8.js to index-lakeville-9.js

--- js/index-lakeville-9.js
+++ js/index-lakeville-9.js
@@ -4,7 +4,7 @@
       // Inject your stylesheet
       const link = document.createElement("link");
       link.rel = "stylesheet";
-      link.href = "https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/css/index-lakeville-5.css";
+      link.href = "https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/css/index-lakeville-6.css";
       document.head.appendChild(link);
     });
   }
