1396a14c32036c273bcf757c31397af2c04ee8b5
keeganryanrealty <keegan.ryan@exprealty.com>
2025-07-09 12:35:02 -0500

    Update and rename lakeville-upsizers-8.js to lakeville-upsizers-9.js

--- js/lakeville-upsizers-9.js
+++ js/lakeville-upsizers-9.js
@@ -1,7 +1,7 @@
 (function () {
   if (window.location.pathname === "/pages/4-bed-homes-lakeville-mn") {
     document.addEventListener("DOMContentLoaded", function () {
-      fetch("https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/lakeville-upsizers-8.html")
+      fetch("https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/lakeville-upsizers-9.html")
         .then(response => response.text())
         .then(html => {
           const wrapper = document.createElement("div");
