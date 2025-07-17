454efc01b90a38bd05f292bc350438eb312fddf3
keeganryanrealty <keegan.ryan@exprealty.com>
2025-07-17 15:00:02 -0500

    Update and rename quiz-16.js to quiz-17.js

--- js/quiz-17.js
+++ js/quiz-17.js
@@ -6,7 +6,7 @@
   const container = document.createElement("div");
   container.id = "quiz-container";
 
-  fetch("https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/quiz-16.html")
+  fetch("https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/quiz-17.html")
     .then(res => res.text())
     .then(async html => {
       container.innerHTML = html;
