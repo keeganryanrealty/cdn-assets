2817315a9ff792d1cabf31b1cf010bf30f17dc34
keeganryanrealty <keegan.ryan@exprealty.com>
2025-07-18 15:44:29 -0500

    Update and rename quiz-58.js to quiz-59.js

--- js/quiz-59.js
+++ js/quiz-59.js
@@ -29,7 +29,7 @@ function showQuizExitModal() {
   if (document.getElementById("quiz-exit-modal")) return; // Don't duplicate
   document.body.style.overflow = "hidden";
 
-  fetch("https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/quiz-exit-modal-01.html?v=3")
+  fetch("https://cdn.jsdelivr.net/gh/keeganryanrealty/cdn-assets@main/html/quiz-exit-modal-02.html")
     .then(res => res.text())
     .then(html => {
       const wrapper = document.createElement("div");
